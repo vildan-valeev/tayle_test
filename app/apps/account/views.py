47323f4140b07a41ebbfcf3399fb7933fb0460ac
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2022-05-20 14:10:01 +0500

    поправил показ счетов на фронте - в дизайне

--- app/apps/account/views.py
+++ app/apps/account/views.py
@@ -4,6 +4,9 @@ from django.views.generic import DetailView
 from .models import Account
 
 # Create your views here.
+from ..balance.models import Bill
+
+
 class AccountDetail(LoginRequiredMixin, DetailView):
     model = Account
     context_object_name = "account"
@@ -11,3 +14,10 @@ class AccountDetail(LoginRequiredMixin, DetailView):
 
     def get_object(self):
         return self.request.user
+
+    def get_context_data(self, **kwargs):
+        context = super(AccountDetail, self).get_context_data()
+        print(kwargs)
+        bills = Bill.objects.filter(user=self.get_object())
+        context['bills'] = bills.values()
+        return context
