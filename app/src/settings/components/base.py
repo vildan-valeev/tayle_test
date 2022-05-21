c631398844d01ac6af247a8618797793d3fc11c8
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2022-05-22 00:01:05 +0500

    disable timezone

--- app/src/settings/components/base.py
+++ app/src/settings/components/base.py
@@ -108,7 +108,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')  # сбор всей статики в одну директорию для вебсервера nginx
