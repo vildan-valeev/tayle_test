4e250c551b681e314b09b4c6205049569ea88f41
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2022-05-21 15:06:24 +0500

    refactoring

--- app/apps/account/models.py
+++ app/apps/account/models.py
@@ -3,21 +3,47 @@ from django.db import models
 
 
 class Account(AbstractUser):
-    first_name = models.CharField(max_length=200, null=True, blank=True)
-    last_name = models.CharField(max_length=200, null=True, blank=True)
-    company = models.CharField(max_length=200, null=True, blank=True)
-    phone = models.CharField(max_length=20, null=True, blank=True)
-    country = models.CharField(max_length=200, null=True, blank=True)
-    job = models.CharField(max_length=200, null=True, blank=True)
+    first_name = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True,
+        verbose_name="Имя"
+    )
+    last_name = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True,
+        verbose_name="Фамилия"
+    )
+    company = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True,
+        verbose_name="Компания"
+    )
+    phone = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True,
+        verbose_name="Телефон"
+    )
+    country = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True,
+        verbose_name="Страна"
+    )
+    job = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True,
+        verbose_name="Работа"
+    )
 
-    def get_first_name(self):
-        return self.first_name
-
-    def get_last_name(self):
-        return self.last_name
-
-    def get_short_name(self):
-        return self.email
+    def get_full_name(self):
+        if self.first_name is None or self.last_name is None:
+            return self.username
+        return f"{self.first_name} {self.last_name}"
 
     def __str__(self):
         return self.username
