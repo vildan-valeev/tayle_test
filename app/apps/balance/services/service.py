1cb98ad573a2bacb94793820fbc18528f02a75da
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2022-05-21 16:44:03 +0500

    add description for code

--- app/apps/balance/services/service.py
+++ app/apps/balance/services/service.py
@@ -2,6 +2,12 @@ from django.db.models import Min
 
 from apps.balance.models import AccountBillTransaction, BillTransaction
 
+"""
+Не используется явно принцип "жирные модели" и "жирные вьюхи". 
+Все что самописное, бизнес-логика и тд - все в отдельные функции, классы, слои.
+Необходимо для удобства тестирования.
+"""
+
 
 def transaction_many_to_one(instance: AccountBillTransaction):
     """Бизнес-логика транзакции при выборе нескольких счетов"""
@@ -12,7 +18,7 @@ def transaction_many_to_one(instance: AccountBillTransaction):
     min_need = instance.amount / len(bills)
     print(min_need, min_bill.get("balance__min"))
     # если остаток на самом минимальном счете превышает min_need(сумма разделенная на количество счетов),
-    # то значит средств на всех счетах достаточно для снятия суммы. Пример в t_transaction.py
+    # то значит средств на всех счетах достаточно для снятия суммы. Пример в test_transaction_example.py
     if min_bill.get("balance__min") >= min_need:
         for bill in bills:
             BillTransaction.objects.create(
